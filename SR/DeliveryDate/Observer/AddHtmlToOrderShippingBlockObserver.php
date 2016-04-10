739b7b70d854efc35a71aada25164bf20c2da090
Sohel Rana <sohelrana09@gmail.com>
2016-04-11 00:03:01 +0600

    Add delivery information in customer order view page

---
+++ SR/DeliveryDate/Observer/AddHtmlToOrderShippingBlockObserver.php
@@ -0,0 +1,46 @@
+<?php
+namespace SR\DeliveryDate\Observer;
+
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Event\ObserverInterface;
+
+class AddHtmlToOrderShippingBlockObserver implements ObserverInterface
+{
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
+
+    /**
+     * @param \Magento\Framework\ObjectManagerInterface $objectmanager
+     */
+    public function __construct(\Magento\Framework\ObjectManagerInterface $objectmanager)
+    {
+        $this->_objectManager = $objectmanager;
+    }
+
+    public function execute(EventObserver $observer)
+    {
+        if($observer->getElementName() == 'sales.order.info') {
+            $orderShippingViewBlock = $observer->getLayout()->getBlock($observer->getElementName());
+            $order = $orderShippingViewBlock->getOrder();
+            $localeDate = $this->_objectManager->create('\Magento\Framework\Stdlib\DateTime\TimezoneInterface');
+            $formattedDate = $localeDate->formatDate(
+                $localeDate->scopeDate(
+                    $order->getStore(),
+                    $order->getDeliveryDate(),
+                    true
+                ),
+                \IntlDateFormatter::MEDIUM,
+                false
+            );
+
+            $deliveryDateBlock = $this->_objectManager->create('Magento\Framework\View\Element\Template');
+            $deliveryDateBlock->setDeliveryDate($formattedDate);
+            $deliveryDateBlock->setDeliveryComment($order->getDeliveryComment());
+            $deliveryDateBlock->setTemplate('SR_DeliveryDate::order_info_shipping_info.phtml');
+            $html = $observer->getTransport()->getOutput() . $deliveryDateBlock->toHtml();
+            $observer->getTransport()->setOutput($html);
+        }
+    }
+}
