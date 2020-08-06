08ab966dbd5caee640664e7253d3598cf2fb1151
Sohel Rana <sohelrana09@gmail.com>
2020-08-06 19:55:21 +0600

    Compatible for M2.4

--- SR/DeliveryDate/view/frontend/requirejs-config.js
+++ SR/DeliveryDate/view/frontend/requirejs-config.js
@@ -8,6 +8,9 @@ var config = {
         mixins: {
             'Magento_Checkout/js/view/shipping': {
                 'SR_DeliveryDate/js/mixin/shipping-mixin': true
+            },
+            'Amazon_Payment/js/view/shipping': {
+                'SR_DeliveryDate/js/mixin/shipping-mixin': true
             }
         }
     }
